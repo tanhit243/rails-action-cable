e95ed2db1134b31623f7f7027a933b023fa2d40f
TA <tanhit243@gmaill.com>
2021-10-06 20:56:52 +0700

    Task refactor code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,17 @@
+# frozen_string_literal: true
+
+# CommentsController
 class CommentsController < ApplicationController
   before_action :set_message
 
   def create
-    @comment = Comment.create! content: params[:comment][:content], message: @message, user: current_user
+    @comment = Comment.create! content: params[:comment][:content],
+                               message: @message, user: current_user
   end
 
   private
 
   def set_message
-    @message = Message.find(params[:message_id])
+    @message = Message.find params[:message_id]
   end
 end
