addfe8bbdec45230fe8ffc477a1b8a6e6bd27cf1
TA <tanhit243@gmaill.com>
2021-10-15 21:34:00 +0700

    Task add rubocop ci

--- app/jobs/comment_relay_job.rb
+++ app/jobs/comment_relay_job.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# CommentRelayJob
 class CommentRelayJob < ApplicationJob
   queue_as :default
 
--- test/controllers/examples_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ExamplesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get examples_index_url
-    assert_response :success
-  end
-end
